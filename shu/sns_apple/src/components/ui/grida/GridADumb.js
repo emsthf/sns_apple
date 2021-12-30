d5928049e2a74f7f235309fe28b4117e6c4f63e1
shushuxx <wsy0912@gmail.com>
2021-12-30 17:28:55 +0900

    upup

---
+++ shu/sns_apple/src/components/ui/grida/GridADumb.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function GridADumb () {
+    return ( 
+
+        <div className="post__text">
+            <div className="post__thumb">
+                <div className="background-img" style={{ background: `url('http://via.placeholder.com/470X650')`}}></div>
+                <Link className="link-overlay" href="single-1.html"></Link>
+            </div>
+        </div>
+     );
+}
+
+export default GridADumb;
