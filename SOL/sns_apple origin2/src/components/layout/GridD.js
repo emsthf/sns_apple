2b93c2f58d6fad439f1e38396d981110909e436a
이정훈 <lllii1111@naver.com>
2022-01-04 11:03:26 +0900

    edit up1

--- SOL/sns_apple origin2/src/components/layout/GridD.js
+++ SOL/sns_apple origin2/src/components/layout/GridD.js
@@ -1,30 +1,94 @@
-import React from 'react';
-import Title from '../ui/grid/Title';
-import PostDumb from '../ui/gridd/PostDumb';
-import PostMeta from '../ui/gridd/PostMeta';
-import PostSmalld from '../ui/gridd/PostSmalld';
-import PostTextd from '../ui/gridd/PostTextd';
+import React from "react";
+import Title from "../ui/grid/Title";
+import { Link } from "react-router-dom";
 
 function GridD() {
-   return (   
-      <div className="atbs-block atbs-block--fullwidth atbs-post-grid-d">
-         <div className="container">
-            <div className="post-grid-d__inner">
-                  <Title />
-               <div className="post-grid-d__wrap">
-                  <div className="big-post-grid-d">
-                     <article className="post post--horizontal post--horizontal-sm">
-                        <PostMeta />
-                        <PostDumb />
-                           <PostTextd />
-                     </article>
+  return (
+    <div className="atbs-block atbs-block--fullwidth atbs-post-grid-d">
+      <div className="container">
+        <div className="post-grid-d__inner">
+          <Title />
+          <div className="post-grid-d__wrap">
+            <div className="big-post-grid-d">
+              <article className="post post--horizontal post--horizontal-sm">
+                <div className="post-meta">
+                  <div className="meta__item">
+                    <p className="title">Date</p>
+                    <time
+                      className="time published"
+                      title="August 20, 2016 at 08:53 am"
+                    >
+                      jun 26, 2018
+                    </time>
                   </div>
-                     <PostSmalld />
-               </div>
+                  <div className="meta__item entry-tags ">
+                    <p className="title">Tags</p>
+                    <ul className="social-list social-list--md list-horizontal">
+                      <li>
+                        <Link to="tags.html">video</Link>
+                      </li>
+                      <li>
+                        <Link to="tags.html">travel</Link>
+                      </li>
+                      <li>
+                        <Link to="tags.html">fashion</Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="post__thumb">
+                  <div className="background-img"></div>
+                  <Link className="link-overlay" to="single-1.html"></Link>
+                </div>
+                <div className="post__text">
+                  <div className="post-meta-top">
+                    <div className="author">
+                      <Link to="author.html" className="author__avatar">
+                        <img
+                          src="http://via.placeholder.com/40x40"
+                          alt="file not found"
+                        />
+                      </Link>
+                      <div className="author__text ">
+                        <Link className="author__name" to="author.html">
+                          Adeline Eirene
+                        </Link>
+                        <p className="">London</p>
+                      </div>
+                    </div>
+                  </div>
+                  <h3 className="post__title typescale-4 takei-title">
+                    <Link to="single-1.html">
+                      The Only Thing You Can Get in a Hurry Is Trouble
+                    </Link>
+                  </h3>
+                </div>
+              </article>
             </div>
-         </div>
+            <div className="small-post-grid-d">
+              <article className="post post--horizontal flexbox flexbox--middle">
+                <div className="post__thumb">
+                  <div className="background-img"></div>
+                  <Link className="link-overlay" to="single-1.html"></Link>
+                </div>
+                <div className="post__text flexbox__item">
+                  <h3 className="post__title takei-title typescale-2">
+                    <Link to="single-1.html">
+                      A Harmful Truth is Better Than a Useful Lie
+                    </Link>
+                  </h3>
+                  <div className="post__excerpt">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiumod tempor incididunt ut labore.
+                  </div>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
       </div>
-   );
+    </div>
+  );
 }
 
 export default GridD;
--- SOL/sns_apple origin2/src/components/layout/Header.js
+++ SOL/sns_apple origin2/src/components/layout/Header.js
@@ -1,18 +1,172 @@
-import React from 'react';
-import MobileHeader from '../ui/header/MobileHeader.js';
-import Nav from '../ui/header/Nav';
+import React from "react";
+
+import { Link } from "react-router-dom";
+import logo from "../../img/logo_03.png";
 
 function Header() {
-   return ( 
-      <header className="site-header">
-         <div className="container">
-            <div className="row">
-               <MobileHeader />
-               <Nav />
+  return (
+    <header className="site-header">
+      <div className="container">
+        <div className="row">
+          <div
+            id="atbs-mobile-header"
+            className="takei-mobile-header visible-xs visible-sm"
+          >
+            <div className="mobile-header__inner mobile-header__inner--flex">
+              <div className="header-branding flexbox__item header-branding--mobile mobile-header__section flexbox text-left">
+                <div className="button-head">
+                  <Link
+                    to=""
+                    className="offcanvas-menu-toggle mobile-header-btn js-atbs-offcanvas-toggle"
+                  >
+                    <div className="button-head"></div>
+                  </Link>
+                </div>
+                <div className=" flexbox__item text-left">
+                  <div className="header-logo header-logo--mobile  text-left">
+                    <Link to="">
+                      <img src="img/logo_03.png" alt="logo" />
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className=" flexbox__item mobile-header__section text-right">
+                <div className="navigation-bar__section lwa lwa-template-modal">
+                  <Link
+                    to=""
+                    className="navigation-bar__login-btn navigation-bar-btn"
+                    data-toggle="modal"
+                    data-target="#login-modal"
+                  >
+                    <i className="mdicon mdicon-person"></i>
+                  </Link>
+                  <button
+                    type="submit"
+                    className="navigation-bar-btn js-search-dropdown-toggle"
+                  >
+                    <i className="mdicon mdicon-search"></i>
+                  </button>
+                </div>
+              </div>
             </div>
-         </div>
-      </header>
-   );
+          </div>
+          <nav className="navigation-bar navigation-bar--fullwidth hidden-xs hidden-sm js-sticky-header-holder">
+            <div className="container">
+              <div className="navigation-bar__inner">
+                <div className="navigation-bar__section">
+                  <div className="header-branding header-branding--mobile mobile-header__section text-left">
+                    <div className="header-logo header-logo--mobile flexbox__item text-left">
+                      <Link to="/">
+                        <img src={logo} alt="logo" />
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+                <div className="navigation-wrapper navigation-bar__section js-priority-nav text-left">
+                  <ul
+                    id="menu-main-menu-1"
+                    className="navigation navigation--main navigation--inline navigation--main-menu"
+                  >
+                    <li className=" menu-item-cat-1 navigation__active menu-item-has-children">
+                      <Link to="/">Home</Link>
+                      <ul className="sub-menu">
+                        <li>
+                          <Link to="/">home-1</Link>
+                        </li>
+                        <li>
+                          <Link to="/">home-2</Link>
+                        </li>
+                        <li>
+                          <Link to="/">home-3</Link>
+                        </li>
+                      </ul>
+                    </li>
+                    <li className="menu-item-cat-1 menu-item-has-children">
+                      <Link to="/">Single</Link>
+                      <ul className="sub-menu">
+                        <li>
+                          <Link to="/">single-1</Link>
+                        </li>
+                        <li>
+                          <Link to="/">single-2</Link>
+                        </li>
+                      </ul>
+                    </li>
+                    <li className="menu-item-cat-1 menu-item-has-children">
+                      <Link to="/">Archive</Link>
+                      <ul className="sub-menu">
+                        <li className="menu-item-has-children">
+                          <Link to="/">Category</Link>
+                          <ul className="sub-menu">
+                            <li>
+                              <Link to="/">category-1</Link>
+                            </li>
+                            <li>
+                              <Link to="/">category-2</Link>
+                            </li>
+                            <li>
+                              <Link to="/">category-3</Link>
+                            </li>
+                          </ul>
+                        </li>
+                        <li>
+                          <Link to="/">Arcihve Tags</Link>
+                        </li>
+                        <li>
+                          <Link to="/">Author</Link>
+                        </li>
+                      </ul>
+                    </li>
+                    <li className="menu-item-cat-1 menu-item-has-children">
+                      <Link to="/">Page</Link>
+                      <ul className="sub-menu">
+                        <li>
+                          <Link to="/">Page</Link>
+                        </li>
+                        <li>
+                          <Link to="/">Page No Sidebar</Link>
+                        </li>
+                        <li>
+                          <Link to="/">Search</Link>
+                        </li>
+                        <li>
+                          <Link to="/">Typography</Link>
+                        </li>
+                        <li>
+                          <Link to="/">404</Link>
+                        </li>
+                      </ul>
+                    </li>
+                    <li className="menu-item-cat-1">
+                      <Link to="/">contact</Link>
+                    </li>
+                  </ul>
+                </div>
+                <div className="navigation-bar__section ">
+                  <div className="navigation-bar__section lwa lwa-template-modal">
+                    <Link
+                      to="/"
+                      className="navigation-bar__login-btn navigation-bar-btn"
+                      data-toggle="modal"
+                      data-target="#login-modal"
+                    >
+                      <i className="mdicon mdicon-person"></i>
+                    </Link>
+                    <button
+                      type="submit"
+                      className="navigation-bar-btn js-search-dropdown-toggle"
+                    >
+                      <i className="mdicon mdicon-search"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
